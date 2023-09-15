fe83d35dd41f283ebd0286b97f3051f0d06690ed
Chuah Wen Juin <chuahwj00@yahoo.com>
2023-09-15 17:49:43 +0800

    Initialize repository

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+import ChartType from "./chart/ChartType.vue";
+import Indicator from "./chart/Indicator.vue";
+import Theme from "./chart/Theme.vue";
+import Language from "./chart/Language.vue";
+import YAxis from "./chart/YAxis.vue";
+import Tooltip from "./chart/Tooltip.vue";
+import Timezone from "./chart/Timezone.vue";
+import Overlay from "./chart/Overlay.vue";
+import LoadMore from "./chart/LoadMore.vue";
+import Update from "./chart/Update.vue";
+</script>
+
+<template>
+  <div id="app" class="app">
+    <!-- <ChartType />
+    <Theme />
+    <YAxis />
+    <Language />
+    <Tooltip />
+    <Timezone /> -->
+    <Indicator />
+    <!-- <Overlay />
+    <LoadMore />
+    <Update /> -->
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #f2f3f5;
+}
+
+div,
+p {
+  box-sizing: border-box;
+}
+
+p {
+  margin: 0;
+}
+
+.app {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 15px;
+}
+.k-line-chart-container {
+  display: flex;
+  flex-direction: column;
+  margin: 15px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+  background-color: #ffffff;
+  width: 620px;
+  height: 440px;
+  padding: 16px 6px 16px 16px;
+}
+
+.k-line-chart-title {
+  margin: 0;
+  color: #252525;
+  padding-bottom: 10px;
+}
+
+.k-line-chart {
+  display: flex;
+  flex: 1;
+}
+.k-line-chart-menu-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+  font-size: 12px;
+  color: #606060;
+}
+.k-line-chart-menu-container button {
+  cursor: pointer;
+  background-color: #1677ff;
+  border-radius: 2px;
+  margin-right: 8px;
+  height: 24px;
+  line-height: 26px;
+  padding: 0 6px;
+  font-size: 12px;
+  color: #fff;
+  border: none;
+  outline: none;
+}
+</style>
---
+++ src/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="k-line-chart-container">
+    <h3 class="k-line-chart-title">{{ title }}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChartLayout",
+  props: {
+    title: String,
+  },
+};
+</script>
